4ad196f0da924b7849088143a0d5dd3d285bdbf7
Chandan <chandanmohanty11111>
2021-10-09 19:09:16 +0530

    LinkedList  Queue And Stack Added

---
+++ src/com/company/Queue.java
@@ -0,0 +1,38 @@
+package com.company;
+
+
+public class Queue<T> {
+    private LinkedList<T> myLinkedList;
+
+    public Queue() {
+        myLinkedList = new LinkedList<>();
+    }
+
+    public void enqueue(T data) {
+        myLinkedList.append(data);
+    }
+
+    public T dequeue() {
+        T top = myLinkedList.head.getData();
+        myLinkedList.pop();
+        return top;
+    }
+
+    public void print() {
+        myLinkedList.display();
+    }
+
+    public boolean isEmpty() {
+        if (myLinkedList.head == null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public int size() {
+        return myLinkedList.size();
+    }
+}
+
+
---
+++ src/com/company/Stack.java
@@ -0,0 +1,40 @@
+package com.company;
+
+
+public class Stack<T> {
+    public LinkedList<T> myLinkedList;
+
+    public Stack() {
+        myLinkedList = new LinkedList<T>();
+    }
+
+    public void push(T data) {
+        myLinkedList.insert(data);
+    }
+
+    public void print() {
+        myLinkedList.display();
+    }
+
+    public T peak() {
+        return myLinkedList.head.getData();
+    }
+
+    public T pop() {
+        T top = myLinkedList.head.getData();
+        myLinkedList.pop();
+        return top;
+    }
+
+    public boolean isEmpty() {
+        if (myLinkedList.head == null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public int size() {
+        return myLinkedList.size();
+    }
+}
